8a0ba077a1082a373c4951890b8c7a625ee101ad
lunaInsidious <itsukitakatsuno@gmail.com>
2025-07-18 04:49:55 +0900

    Updating main entry point to run CLI when executed directly

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+#!/usr/bin/env node
+
+import { runCLI } from "./cli/index.js";
+
+// Only run CLI if this file is executed directly
+if (import.meta.url === `file://${process.argv[1]}`) {
+	runCLI(process.argv).catch((error) => {
+		console.error("Unexpected error:", error);
+		process.exit(1);
+	});
+}
